8172fd25197536a77ceba77ce79489be5bcf79d5
harika0103 <harikapati78@gmail.com>
2025-07-17 11:50:35 +0000

    res

--- resource.tf
+++ resource.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "terra-azure" {
 resource "azurerm_storage_account" "terraStorage" {
   name                     = "hari0103strgacc"    # must be globally unique
   resource_group_name      = azurerm_resource_group.terra-azure.name
-  location                 = azurerm_resource_group.terraAzure.location
+  location                 = azurerm_resource_group.terra-azure.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
