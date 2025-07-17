4a221e8d574c468be9174e2e15d6d264cdf3b621
harika0103 <harikapati78@gmail.com>
2025-07-17 11:45:50 +0000

    res

--- resource.tf
+++ resource.tf
@@ -2,3 +2,14 @@ resource "azurerm_resource_group" "terra-azure" {
   name     = "casestudy-Terraform"
   location = "East US"
 }
+resource "azurerm_storage_account" "terraStorage" {
+  name                     = "hari0103strgacc"    # must be globally unique
+  resource_group_name      = azurerm_resource_group.terraAzure.name
+  location                 = azurerm_resource_group.terraAzure.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "CaseStudy"
+  }
+}
